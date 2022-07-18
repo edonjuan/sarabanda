dfa5619e84b5ea63e799050bdbde1aa96b5e29ab
Eduardo Donjuan <mario.donjuan@uteq.edu.mx>
2022-07-18 10:21:02 -0500

    Update slave.ino

--- mesh/slave.ino
+++ mesh/slave.ino
@@ -1,5 +1,4 @@
-﻿// slave - jaula
-//com6 Jaula 
+﻿// slave Mesh
 
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
@@ -12,7 +11,7 @@
 #define Botton  0
 #define pir     5 
 #define ldr     A0
-#define pir_rate_min 1
+#define msg_rate 10
 #define LOADCELL_DOUT_PIN 14
 #define LOADCELL_SCK_PIN  12
 
@@ -21,21 +20,13 @@
 #define   MESH_PASSWORD   "somethingSneaky"
 #define   MESH_PORT       5555
 // MASTER
-#define COM4BLUE  3822969472
+#define MASTER  3822969472
 
 // Global variables
-int  conteo;  //pir
+unsigned int conteo;  //pir
+unsigned short check;
+unsigned short tenSec;
 
-short  REF_A, REF_B;// cambio de referencia 
-int mayor_A=-999; // declaracion del numero mayor y menor de la celda A 
-int menor_A=999; 
-
-int mayor_B=-999; // declaracion del numero mayor y menor de la celda B 
-int menor_B=999; 
-
-int diferencia_A, diferencia_B, gr_A, gr_A2, gr_B, gr_B2;
-int diferenciatotal_A, diferenciatotal_B;
- 
 // Objetct declaration
 HX711 scale_A;
 HX711 scale_B;
@@ -52,76 +43,62 @@ void scale_a(void);
 void scale_b(void);
 
 // Task declaration
-Task taskSendMessage( TASK_MINUTE *pir_rate_min  , TASK_FOREVER, &sendMessage );
+Task taskSendMessage( TASK_SECOND *msg_rate  , TASK_FOREVER, &sendMessage );
 
 void sendMessage() {
 
   float t, h;
-  int m, l, a, b;
+  int l, a, b;
+  unsigned int m;
 
-  digitalWrite(LED_BUILTIN, !HIGH);
-  
-  // Temp & Hum
-  t=dht.readTemperature();
-  h=dht.readHumidity();  
-  if (isnan(h) || isnan(t)) {
-   Serial.println(F("Failed to read DHT!"));
+  if(tenSec>=5)
+  {
+    tenSec=0;
+    check = 0;
   }
-
-  // Mov
-  m = conteo;
-  conteo = 0;
-
-  // Luz
-  l = analogRead(ldr);
-
-  // A_scale
-  if(REF_A){
-     diferenciatotal_A = 0;// si hay cambio de ref la diferencia se iguala a 0 
+  else
+  {
+    tenSec++;
   }
-  else{        
-     diferenciatotal_A = mayor_A-menor_A; //si no hay cambio de ref se hace la diferencia 
+  Serial.print("iteracion: ");
+  Serial.println(tenSec);
+
+  if(check == 0)
+  {  
+      digitalWrite(LED_BUILTIN, !HIGH);
+      
+      // Temp & Hum
+      t=dht.readTemperature();
+      h=dht.readHumidity();  
+      if (isnan(h) || isnan(t)) {
+       Serial.println(F("Failed to read DHT!"));
+      }
+    
+      // Mov
+      m = conteo;
+    
+      // Luz
+      l = analogRead(ldr);
+    
+      scale_A.get_units(10);
+      a = scale_A.get_units(10);
+    
+      delay(500);
+      
+      scale_B.get_units(10);
+      b = scale_B.get_units(10);
+    
+      // msg contruct
+      String  msg = String (t) + "T";
+      msg += String (h) + "H";
+      msg += String (m) + "M";
+      msg += String (l) + "L";
+      msg += String (a) + "A";
+      msg += String (b) + "B";
+      Serial.println(msg);
+      mesh.sendSingle (MASTER, msg); // manda mensaje a blu 
+      digitalWrite(LED_BUILTIN, !LOW);
   }
-
-  if(diferenciatotal_A<0){
-    a = 0;
-  }
-  else{
-    a = diferenciatotal_A;
-  } 
-  mayor_A=-999;
-  menor_A=999; 
-
-
-  // A_scale
-  if(REF_B){
-     diferenciatotal_B = 0;// si hay cambio de ref la diferencia se iguala a 0 
-  }
-  else{        
-     diferenciatotal_B = mayor_B-menor_B; //si no hay cambio de ref se hace la diferencia 
-  }
-
-  if(diferenciatotal_B<0){
-    b= 0;
-  }
-  else{
-    b = diferenciatotal_B;
-  } 
-  mayor_B=-999;
-  menor_B=999; 
-
-  // msg contruct
-  String  msg = String (t) + "T";
-  msg += String (h) + "H";
-  msg += String (m) + "M";
-  msg += String (l) + "L";
-  msg += String (a) + "A";
-  msg += String (b) + "B";
-  Serial.println(msg);
-
-   // msg += mesh.getNodeId();
-  mesh.sendSingle (COM4BLUE, msg ); // manda mensaje a blu   
-  digitalWrite(LED_BUILTIN, !LOW);
 }
      
 
@@ -147,82 +124,21 @@ void setup() {
 
   // Network
   mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
 }
           
 void loop() {
-            // it will run the user scheduler as well
   mesh.update();
-  // Lectura de celdas (rate = ?)    
-  scale_a();
-  scale_b();  
 }
 
-// Function declaration
-void scale_a(void){
-
-   // Lectura SCALE_A   
-   delay(500); // wait mux
-   scale_A.get_units(10);
-   gr_A = scale_A.get_units(10);
-   diferencia_A = gr_A2 - gr_A; //se sacan las diferencias de la celda   
-
-   if ((diferencia_A>=0)&&(diferencia_A<5)) {
-        if(gr_A>mayor_A){
-        mayor_A=gr_A;
-        }
-        if(gr_A<menor_A){
-        menor_A=gr_A;
-        }
-        
-        REF_A=0; // no cambios bruscos
-   }
-   else if (diferencia_A>=5){
-         mayor_A=-999;
-         menor_A=999; 
-         REF_A=1; // si cambios bruscos
-    }
-           
-    else if(diferencia_A<-5){ // se quita mucho peso celda A-B
-         mayor_A=-999;
-         menor_A=999; 
-         REF_A=1;
-    }              
-    gr_A2 = gr_A;  
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("Received from %u msg=%s\n", from, msg.c_str());
+  if(from == MASTER)
+  {
+    check = 1;
+    tenSec=0;
+  }
 }
-
-
-void scale_b(void){
-
-   // Lectura SCALE_B   
-   delay(500); // wait mux
-   scale_B.get_units(10);
-   gr_B = scale_B.get_units(10);
-   diferencia_B = gr_B2 - gr_B; //se sacan las diferencias de la celda   
-
-   if ((diferencia_B>=0)&&(diferencia_B<5)) {
-        if(gr_B>mayor_B){
-        mayor_B=gr_B;
-        }
-        if(gr_B<menor_B){
-        menor_B=gr_B;
-        }
-        
-        REF_B=0; // no cambios bruscos
-   }
-   else if (diferencia_B>=5){
-         mayor_B=-999;
-         menor_B=999; 
-         REF_B=1; // si cambios bruscos
-    }
-           
-    else if(diferencia_B<-5){ // se quita mucho peso celda A-B
-         mayor_B=-999;
-         menor_B=999; 
-         REF_B=1;
-    }              
-    gr_B2 = gr_B;  
-}
-
 
 void IRAM_ATTR set_tare() { //interrupcion para inicializar taras en 0 
 
@@ -242,10 +158,6 @@ void IRAM_ATTR set_tare() { //interrupcion para inicializar taras en 0
     scale_B.set_scale(500);
     scale_B.get_units(10);
     scale_B.tare();
-
-    // Clear control variables
-    diferencia_A=0; 
-    diferencia_B=0; 
 }
 
 void IRAM_ATTR pir_action() {
